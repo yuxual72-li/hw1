bd1a7fef2ab489df22085749387cbdaee609b4c7
yuxual72-li <yuxual72@uci.edu>
2026-07-13 22:12:37 -0700

    Create helpers1.h

---
+++ include/include/helpers1.h
@@ -0,0 +1,16 @@
+// Declare all helper functions for hw1 in this file
+#include <iostream>
+//-L
+int countLines(std::istream& in);
+
+//-S
+int countTotalSymbols(std::istream& in, bool verbose);
+
+//-N
+int countNumberOccur(std::istream& in, bool verbose);
+
+//-C
+int contractSpaces(std::istream& in, int numSpaces);
+
+//-W
+int countBlankLines(std::istream& in);
